1dbd4be295e7cae769a6dfaeff95c4a680f2d678
Agustinus <57866141+lgarin211@users.noreply.github.com>
2023-01-10 03:51:31 +0000

    pall

--- src/PPTI15/Ngide/i1.java
+++
@@ -1,9 +0,0 @@
-package PPTI15.Ngide;
-
-public class i1 {
-    public static void main(String[] args) {
-        String A="Ferren";
-        System.out.println(A.charAt(0));
-        // System.out.println();
-    }
-}
